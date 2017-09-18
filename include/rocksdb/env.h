3c5c518fa2884c3f952a3c8147022eb39d730db5
Seth Howell <seth.howell@intel.com>
2017-09-18 14:16:17 -0700

    update command line args for spdk
    passing in a generic options argument to NewSpdkEnv allows us to
    maintain operability when either spdk or rocksdb versions are changed.
    
    Change-Id: I50947023dc29a4aed343438a721481e407501834
    Signed-off-by: Seth Howell <seth.howell@intel.com>

--- include/rocksdb/env.h
+++ include/rocksdb/env.h
@@ -1129,7 +1129,7 @@ Env* NewTimedEnv(Env* base_env);
 
 // Returns a new environment that is used for SPDK environment.
 Env* NewSpdkEnv(Env* base_env, const std::string& fsname, const std::string& confname,
-		const std::string& bdevname, uint64_t cache_size_in_mb);
+		const std::string& bdevname, struct spdk_opts *opts, size_t size);
 
 // Initializes a thread for SpdkEnv processing.
 void SpdkInitializeThread(void);
--- tools/db_bench_tool.cc
+++ tools/db_bench_tool.cc
@@ -712,6 +712,7 @@ DEFINE_int32(table_cache_numshardbits, 4, "");
 DEFINE_string(spdk, "", "Name of SPDK configuration file");
 DEFINE_string(spdk_bdev, "", "Name of SPDK blockdev to load");
 DEFINE_uint64(spdk_cache_size, 4096, "Size of SPDK filesystem cache (in MB)");
+DEFINE_int32(spdk_shared_mem, -1, "Shared memory region id from stub application");
 
 #ifndef ROCKSDB_LITE
 DEFINE_string(env_uri, "", "URI for registry Env lookup. Mutually exclusive"
@@ -1103,6 +1104,12 @@ class ReportFileOpEnv : public EnvWrapper {
 };
 
 }  // namespace
+
+// definition of spdk_opt structure for initializing spdk_env
+struct spdk_opts{
+	uint64_t cache_size;
+	int32_t shm_id;
+};
 
 // Helper for quickly generating random data.
 class RandomGenerator {
@@ -5165,7 +5172,10 @@ int db_bench_tool(int argc, char** argv) {
   }
 
   if (!FLAGS_spdk.empty()) {
-    FLAGS_env = rocksdb::NewSpdkEnv(rocksdb::Env::Default(), FLAGS_db, FLAGS_spdk, FLAGS_spdk_bdev, FLAGS_spdk_cache_size);
+    struct spdk_opts opts;
+    opts.cache_size = FLAGS_spdk_cache_size;
+    opts.shm_id = FLAGS_spdk_shared_mem;
+    FLAGS_env = rocksdb::NewSpdkEnv(rocksdb::Env::Default(), FLAGS_db, FLAGS_spdk, FLAGS_spdk_bdev, &opts, sizeof(struct spdk_opts));
     if (FLAGS_env == NULL) {
       fprintf(stderr, "Could not load SPDK blobfs - check that SPDK mkfs was run "
                       "against block device %s.\n", FLAGS_spdk_bdev.c_str());
